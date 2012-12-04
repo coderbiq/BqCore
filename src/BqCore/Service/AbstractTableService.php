c980b44a6348879c2b045bb32cdff08f39377ba9
Elvis.bi <elvis@dwenzi.com>
2012-12-04 23:29:38 +0800

    创建数据表服务基类

---
+++ src/BqCore/Service/AbstractTableService.php
@@ -0,0 +1,40 @@
+<?php
+namespace BqCore\Service;
+
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\AbstractTableGateway;
+use BqCore\Db\Table\TableInterface;
+use BqCore\Entity\FactoryInterface as EntityFactoryInterface;
+
+abstract class AbstractTableService extends AbstractTableGateway
+    implements TableInterface, EntityFactoryInterface, TableServiceInterface
+{
+    protected $serviceLocator;
+
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+        $this->setServiceLocator($serviceLocator);
+
+        $this->table = static::getTableName();
+
+        $adapter = $this->getServiceLocator()
+            ->get(static::getAdapterServiceName());
+        $this->adapter = $adapter;
+
+        $this->resultSetPrototype = new ResultSet();
+        $this->resultSetPrototype->setArrayObjectPrototype(
+            $this->createEntity());
+
+        return $this;
+
+    }
+
+    public function getServiceLocator() {
+        return $this->serviceLocator;
+    }
+
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator) {
+        $this->serviceLocator = $serviceLocator;
+        return $this;
+    }
+
+}
---
+++ src/BqCore/Service/TableServiceInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace BqCore\Service;
+
+interface TableServiceInterface extends ServiceInterface
+{
+    public static function getAdapterServiceName();
+}
