46513a3f9a83727fc18a7fac67e40e6ab219bc87
Elvis.bi <elvis@dwenzi.com>
2012-12-11 22:00:34 +0800

    创建实体基类；广播数据插入和修改事件

--- src/BqCore/Event/DataEvent.php
+++ src/BqCore/Event/DataEvent.php
@@ -9,7 +9,7 @@ class DataEvent extends Event
     const EVENT_SEARCH      = 'bqcore.data.search';
     const EVENT_SEARCH_POST = 'bqcore.data.search.post';
     const EVENT_INSERT      = 'bqcore.data.insert';
-    const EVENT_INSERT.POST = 'bqcore.data.insert.post';
+    const EVENT_INSERT_POST = 'bqcore.data.insert.post';
     const EVENT_DELETE      = 'bqcore.data.delete';
     const EVENT_DELETE_POST = 'bqcore.data.delete.post';
     const EVENT_CHANGE      = 'bqcore.data.change';
